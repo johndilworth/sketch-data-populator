da6ed121a73dcad8ba8abfb90b92b784ce74baa6
Lukas Ondrej <lukas77me@gmail.com>
2016-07-21 10:34:02 +0100

    added populate table command + preparing data for populating
    - the command doesn't yet populate the layers but the input data is now
    fully processed and prepared for populating

--- table parsing/parseTable.js
+++ table parsing/parseTable.js
@@ -2,7 +2,11 @@ var fs = require('fs');
 var file = fs.readFileSync('./input.tsv', 'utf8');
 
 var tableObject = parseTable(file);
-console.log(JSON.stringify(tableObject, null, 2));
+// console.log(JSON.stringify(tableObject, null, 2));
+
+var flattened = flattenTable(tableObject);
+
+console.log(JSON.stringify(flattened, null, 2));
 
 function parseTable(data) {
 
@@ -68,7 +72,9 @@ function parseTable(data) {
     }
 
     //create grouped table of horizontal entries
-    var groupedTable = [];
+    var groupedTable = {
+        rows: []
+    };
     for (var i = dataY; i < dataY + dataHeight; i++) {
         for (var j = dataX; j < dataX + dataWidth; j++) {
 
@@ -82,56 +88,166 @@ function parseTable(data) {
             //find path to data
             var path = [];
             for (var p = 0; p < dataX - 1; p++) {
-                path.push(table[i][p]);
+                path.push({
+                    title: table[i][p],
+                    type: 'row'
+                });
             }
             for (var p = 0; p < dataY; p++) {
-                path.push(table[p][j]);
+                path.push({
+                    title: table[p][j],
+                    type: 'column'
+                });
             }
-            
+
             //create path structure
-            var parent = groupedTable;
-            for(var p = 0; p < path.length; p++) {
-                
+            var parent = groupedTable.rows;
+            for (var p = 0; p < path.length; p++) {
+
                 //find existing child in parent with same title
                 var existingChild = null;
-                for(var q = 0; q < parent.length; q++) {
-                    if(parent[q].title == path[p]) {
+                for (var q = 0; q < parent.length; q++) {
+                    if (parent[q].title == path[p].title) {
                         existingChild = parent[q];
                         break;
                     }
                 }
-                
+
                 //select next parent
-                if(existingChild) {
-                    parent = existingChild.content;
-                }
-                else {
-                    
+                if (existingChild) {
+                    parent = existingChild[path[p].type + 's'];
+                    if (!parent) parent = existingChild.content;
+                } else {
+
                     //prepare new child that will become next parent
                     var newChild = {
-                        title: path[p]
+                        title: path[p].title
                     };
-                    
+
                     //if it's the last path component, the content is an object
-                    if(p == path.length - 1) {
+                    if (p == path.length - 1) {
                         newChild.content = {};
+                        parent.push(newChild);
+                        parent = newChild.content;
+                    } else {
+                        newChild[path[p].type + 's'] = [];
+                        parent.push(newChild);
+                        parent = newChild[path[p].type + 's'];
                     }
-                    
-                    //otherwise, make content an array
-                    else {
-                        newChild.content = [];
-                    }
-                    
-                    //add new child to parent and make it the next parent
-                    parent.push(newChild);
-                    parent = newChild.content;
                 }
             }
-            
+
             //add value for key to parent
             parent[dataKey] = data;
         }
     }
-    
+
     return groupedTable;
+}
+
+
+function flattenTable(data) {
+
+    return {
+        rowGroups: getRowGroups(data.rows[0]),
+        columnGroups: getColumnGroups(data.rows[0]),
+        cells: getCells(data)
+    };
+    
+    
+    function getCells(data) {
+        
+        var result = {
+            columnsPerRow: 0
+        };
+        
+        //get all cells
+        var allCells = getAllCells(data, data, result);
+        
+        var rows = [];
+        var currentRow = [];
+        for(var i = 0; i < allCells.length; i++) {
+            if(i % result.columnsPerRow == 0) {
+                currentRow = [];
+                rows.push(currentRow);
+            }
+            currentRow.push(allCells[i]);
+        }
+        
+        return rows;
+        
+        
+        function getAllCells(data, parent, result) {
+            
+            var allCells = [];
+            
+            if(data.rows && data.rows.length) {
+                for(var i = 0; i < data.rows.length; i++) {
+                    var row = data.rows[i];
+                    allCells = allCells.concat(getAllCells(row, data, result));
+                }
+            }
+            else if(data.columns && data.columns.length) {
+                for(var i = 0; i < data.columns.length; i++) {
+                    var column = data.columns[i];
+                    allCells = allCells.concat(getAllCells(column, data, result));
+                }
+            }
+            else if(data.content) {
+                
+                //extract cells here
+                allCells.push(data.content);
+                
+                //set columns per row
+                result.columnsPerRow = parent.columns.length;
+            }
+            
+            return allCells;
+        }
+    }
+    
+    function getRowGroups(data) {
+        var groups = [];
+        if (data.rows && data.rows.length) {
+            var group = {
+                title: data.title
+            };
+            groups.push(group);
+            var subGroups = [];
+            for(var i = 0; i < data.rows.length; i++) {
+                subGroups = subGroups.concat(getRowGroups(data.rows[i]));
+            }
+            if(subGroups.length) group.groups = subGroups;
+        }
+        return groups;
+    }
+    
+    function getColumnGroups(data) {
+        var groups = [];
+        
+        //drill down the rows
+        while(data.rows) {
+            data = data.rows[0];
+        }
+        
+        if(data.columns && data.columns.length) {
+            var group = {
+                title: data.title
+            };
+            groups.push(group);
+            var subGroups = [];
+            for(var i = 0; i < data.columns.length; i++) {
+                subGroups = subGroups.concat(getColumnGroups(data.columns[i]));
+            }
+            if(subGroups.length) group.groups = subGroups;
+        }
+        else if(data.content) {
+            var group = {
+                title: data.title
+            };
+            groups.push(group);
+        }
+
+        return groups;
+    }
 }

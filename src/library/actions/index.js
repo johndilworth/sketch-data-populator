0509563cef951d8609c04a1db1703399514e43d5
Lukas Ondrej <lukas77me@gmail.com>
2016-08-07 17:38:51 +0100

    add initial actions library
    - currently just a library wrapper, functionality is not yet implemented

---
+++ src/library/actions/index.js
@@ -0,0 +1,16 @@
+/**
+ * Actions library
+ *
+ * Provides functionality to extract, parse and handle actions.
+ */
+
+
+/**
+ * Extracts conditional actions from a string.
+ *
+ * @param {string} string
+ */
+function extractActions(string) {
+
+  //TODO: add conditional action functionality
+}

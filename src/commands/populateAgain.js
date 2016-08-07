c41a58bd33434dc50c2d48f8e9741a2138583f40
Lukas Ondrej <lukas77me@gmail.com>
2016-08-07 17:46:20 +0100

    add populate again command

--- src/commands/populateAgain.js
+++ src/commands/populateAgain.js
@@ -6,14 +6,40 @@
 
 
 import Context from '../context'
-import * as Data from '../library/data'
-import * as Gui from '../library/gui'
-import * as Layers from '../library/layers'
-import * as Utils from '../library/utils'
+import * as Populator from '../library/populator'
+import Options, * as OPTIONS from '../library/options'
+
+import PopulateWithPreset from './populateWithPreset'
+import PopulateWithJSON from './populateWithJSON'
+import PopulateTable from './populateTable'
 
 
 export default (context) => {
   Context(context)
 
+  //get options
+  let options = Options()
 
+  //check if there is a data path set
+  if(options[OPTIONS.LAST_DATA_PATH]) {
+
+    //get type of last populate command
+    switch(String(options[OPTIONS.LAST_POPULATE_TYPE])) {
+
+      //populate with preset
+      case Populator.POPULATE_TYPE.PRESET:
+        PopulateWithPreset(context, true)
+        break
+
+      //populate with JSON
+      case Populator.POPULATE_TYPE.JSON:
+        PopulateWithJSON(context, true)
+        break
+
+      //populate table
+      case Populator.POPULATE_TYPE.TABLE:
+        PopulateTable(context, true)
+        break
+    }
+  }
 }
